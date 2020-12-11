469863c32f4a509c8f814e11c081b5b453c60b9f
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-12-10 20:24:19 -0700

    Update server.js to proxy requests to services

--- server/server.js
+++ server/server.js
@@ -1,15 +1,44 @@
 const express = require('express');
-const path = require('path');
-
+const {createProxyMiddleware} = require('http-proxy-middleware');
 const app = express();
-const PORT = 3000;
+const path = require('path');
+const port = 80;
 
-app.use('/', express.static(path.join(__dirname, '../public')));
+app.use(express.static('public'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+app.use('/api/reviews/', createProxyMiddleware({
+  target: 'http://localhost:3001',
+  changeOrigin: true
+}));
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+//copy and paste for other routes
+app.use('/api/seller/', createProxyMiddleware({
+  target: 'http://localhost:3002',
+  changeOrigin: true
+}))
+
+app.use('/api/images/', createProxyMiddleware({
+  target: 'http://localhost:3003',
+  changeOrigin: true
+}))
+
+app.use('/api/products/', createProxyMiddleware({
+  target: 'http://localhost:3004',
+  changeOrigin: true
+}))
+
+// app.use('/hashtags/', createProxyMiddleware({
+//   target: 'http://18.189.26.97:4001',
+//   changeOrigin: true
+// }))
+
+// app.use('/users/', createProxyMiddleware({
+//   target: 'http://18.218.58.9:4002/',
+//   changeOrigin: true
+// }))
+
+app.use('/:current', (req, res) => {
+  res.sendFile(path.join(__dirname,'/public/index.html'));
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
