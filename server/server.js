f99411635fd425d73c966476c9f3f395cbef7169
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-12-17 21:09:28 -0800

    Start connecting proxy to products service

--- server/server.js
+++ server/server.js
@@ -7,39 +7,14 @@ const port = 3000;
 
 app.use(express.static('public'));
 
-// app.use('/api/reviews/', createProxyMiddleware({
-//   target: 'http://localhost:3001',
-//   changeOrigin: true
-// }));
+app.use('/products', createProxyMiddleware({
+  target: 'http://localhost:3004/bundle-product-information-service.js',
+  changeOrigin: true,
+  pathRewrite: { '^/products' : '' },
+}));
 
-// //copy and paste for other routes
-// app.use('/api/seller/', createProxyMiddleware({
-//   target: 'http://localhost:3002',
-//   changeOrigin: true
-// }))
-
-// app.use('/api/images/', createProxyMiddleware({
-//   target: 'http://localhost:3003',
-//   changeOrigin: true
-// }))
-
-app.use('/api/products/', createProxyMiddleware({
-  target: 'http://localhost:3004',
-  changeOrigin: true
-}))
-
-// app.use('/hashtags/', createProxyMiddleware({
-//   target: 'http://18.189.26.97:4001',
-//   changeOrigin: true
-// }))
-
-// app.use('/users/', createProxyMiddleware({
-//   target: 'http://18.218.58.9:4002/',
-//   changeOrigin: true
-// }))
-
-app.use('/:current', (req, res) => {
-  res.sendFile(path.join(__dirname,'/public/index.html'));
-})
+app.use('/:id', (req, res) => {
+  res.sendFile(path.join(__dirname,'../public/index.html'));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
