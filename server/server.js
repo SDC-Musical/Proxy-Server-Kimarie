39f82c27a5b1d31ec61b15ec0657409fe3bb645f
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2021-01-01 11:53:43 -0800

    Add morgan for logging

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const {createProxyMiddleware} = require('http-proxy-middleware');
 const app = express();
 const path = require('path');
+const morgan = require('morgan');
 //const port = 80;
 const port = 3000;
 
 app.use(express.static('public'));
+app.use(morgan('dev'));
 
 app.use('/products', createProxyMiddleware({
   target: 'http://localhost:3004/bundle-product-information-service.js',
