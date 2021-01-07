0cccdb79e5f834b18cd222abf887f7fd5b2b33e1
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2021-01-06 20:37:17 -0800

    Add axios, change routes to get individual bundles
    (specs, title, description), remove cruft

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const path = require('path');
+const axios = require('axios');
 const morgan = require('morgan');
 //const port = 80;
 const port = 3000;
@@ -9,16 +10,43 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
-app.use('/products', createProxyMiddleware({
-  target: 'http://localhost:3004/bundle-product-information-service.js',
-  changeOrigin: true,
-  pathRewrite: { '^/products' : '' },
-}));
-
 app.use('/api/products', createProxyMiddleware({
   target: 'http://localhost:3004/',
   changeOrigin: true,
 }));
+
+app.get('/title.js', (req, res) => {
+  axios.get('http://localhost:3004/products/1/title.js')
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.send(err);
+  })
+});
+
+app.get('/description.js', (req, res) => {
+  axios.get('http://localhost:3004/products/1/description.js')
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.send(err);
+  })
+});
+
+app.get('/specs.js', (req, res) => {
+  axios.get('http://localhost:3004/products/1/specs.js')
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.send(err);
+  })
+});
 
 app.get('/:id', (req, res) => {
   res.sendFile(path.join(__dirname,'../public/index.html'));
