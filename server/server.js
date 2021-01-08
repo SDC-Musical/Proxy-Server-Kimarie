0293a11b90fadbc12a4ae0f32f0a75001b8a07b0
Kimarie Haverty-Bozman <50646030+KimarieHB@users.noreply.github.com>
2021-01-07 22:12:28 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,61 @@
+require('newrelic');
 const express = require('express');
-const path = require('path');
-
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
-const PORT = 3000;
+const path = require('path');
+const axios = require('axios');
+const morgan = require('morgan');
+//const port = 80;
+const port = 3000;
 
-app.use('/', express.static(path.join(__dirname, '../public')));
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+app.use('/api/products', createProxyMiddleware({
+  target: 'http://localhost:3004/',
+  changeOrigin: true,
+}));
+
+app.use('/api/update', createProxyMiddleware({
+  target: 'http://localhost:3004/',
+  changeOrigin: true,
+}));
+
+app.get('/title.js', (req, res) => {
+  axios.get('http://localhost:3004/products/1/title.js')
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.send(err);
+  })
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+app.get('/description.js', (req, res) => {
+  axios.get('http://localhost:3004/products/1/description.js')
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.send(err);
+  })
 });
+
+app.get('/specs.js', (req, res) => {
+  axios.get('http://localhost:3004/products/1/specs.js')
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.send(err);
+  })
+});
+
+app.get('/:id', (req, res) => {
+  res.sendFile(path.join(__dirname,'../public/index.html'));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
