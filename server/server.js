4d75d9c417bef9bd4081dc65eb73d04f70fb7bad
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2021-01-13 16:47:15 -0800

    Prepare proxy for deployment
    comment our morgan call and require to be removed in remote server, update proxy request url

--- server/server.js
+++ server/server.js
@@ -4,25 +4,25 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const path = require('path');
 const axios = require('axios');
-const morgan = require('morgan');
-//const port = 80;
-const port = 3000;
+//const morgan = require('morgan');
+const port = 80;
+//const port = 3000;
 
 app.use(express.static('public'));
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 
 app.use('/api/products', createProxyMiddleware({
-  target: 'http://localhost:3004/',
+  target: 'http://18.144.74.123/',
   changeOrigin: true,
 }));
 
 app.use('/api/update', createProxyMiddleware({
-  target: 'http://localhost:3004/',
+  target: 'http://18.144.74.123/',
   changeOrigin: true,
 }));
 
 app.get('/title.js', (req, res) => {
-  axios.get('http://localhost:3004/products/1/title.js')
+  axios.get('http://18.144.74.123/products/1/title.js')
   .then((response) => {
     res.send(response.data);
   })
@@ -33,7 +33,7 @@ app.get('/title.js', (req, res) => {
 });
 
 app.get('/description.js', (req, res) => {
-  axios.get('http://localhost:3004/products/1/description.js')
+  axios.get('http://18.144.74.123/products/1/description.js')
   .then((response) => {
     res.send(response.data);
   })
@@ -44,7 +44,7 @@ app.get('/description.js', (req, res) => {
 });
 
 app.get('/specs.js', (req, res) => {
-  axios.get('http://localhost:3004/products/1/specs.js')
+  axios.get('http://18.144.74.123/products/1/specs.js')
   .then((response) => {
     res.send(response.data);
   })
